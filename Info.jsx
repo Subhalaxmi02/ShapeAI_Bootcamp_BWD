ab04dd52ee8e225087a970aa979f48d73f737fb1
Subhalaxmi Mishra <72184231+Subhalaxmi02@users.noreply.github.com>
2021-06-27 00:35:36 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react" ;
+
+function Info() {
+  return (
+    <div className="note">
+      <h1 class="note"> The mysterious student</h1>
+      <p class="note">Doing B.tech from VSSUT,BURLA</p>
+    </div>
+  );
+}
+
+export default Info;
+     
